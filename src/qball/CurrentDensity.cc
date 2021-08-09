8ab49fd98a4ce52e6154915ff4d910039d25c9a2
yyf <yaoyifan97@126.com>
2021-08-09 11:24:30 -0500

    fix paste error

--- src/qball/CurrentDensity.cc
+++ src/qball/CurrentDensity.cc
@@ -32,8 +32,8 @@
 #include "Base64Transcoder.h"
 
 CurrentDensity::CurrentDensity(const Sample& s, const Wavefunction & wf):
-    ChargeDensity(s), wf_(wf) {
-
+  ChargeDensity(s), wf_(wf){
+  
 }
 
 void CurrentDensity::update_current(EnergyFunctional & energy_functional, const Wavefunction & dwf, bool output) {
@@ -86,178 +86,222 @@ void CurrentDensity::update_current(EnergyFunctional & energy_functional, const 
                 }
             }
 
+            wf_.wfcontext()->dsum('r', vft()->np012loc(), 1, &current[idir][ispin][0], vft()->np012loc());
 
-////////////////////////////////////////////////////////////////////
-void CurrentDensity::twfr(valarray<valarray<valarray<complex<double>>>>& gkswfr,valarray<valarray<complex<double>>>& kswfr,const vector<valarray<double>>& indexAll) const {
-    Wavefunction gwf(wf_);
-    for(int idir = 0; idir < 3; idir++) {
-        for ( int ispin = 0; ispin < wf_.nspin(); ispin++) {
-            for ( int ikp = 0; ikp < wf_.nkp(); ikp++ ) {
-
-                const int ngwloc = wf_.sd(ispin, ikp)->basis().localsize();
-                const int mloc = wf_.sd(ispin, ikp)->c().mloc();
-
-                for ( int n = 0; n < wf_.sd(ispin, ikp)->nstloc(); n++ ) {
-                    for ( int ig = 0; ig < ngwloc; ig++ ) {
-                        gwf.sd(ispin, ikp)->c()[ig + mloc*n] = std::complex<double>(0.0, 1.0)*wf_.sd(ispin, ikp)->basis().kpgx_ptr(idir)[ig]*wf_.sd(ispin, ikp)->c()[ig + mloc*n];
-                    }
-                }
-                if (idir==0) wf_.sd(ispin, ikp)->tmpwfr(*ft(ispin, ikp), *gwf.sd(ispin, ikp),gkswfr,kswfr,indexAll);
-                else wf_.sd(ispin, ikp)->tmpwfr(*ft(ispin, ikp), *gwf.sd(ispin, ikp),gkswfr,indexAll,idir);
-            }
-        }
-    }
-}
-////////////////////////////////////////////////////////////////////
-void CurrentDensity::plot(const Sample * s, const std::string & filename) {
-    using namespace std;
-
-    std::vector<std::vector<double> > global_current(3);
-
-    for(int idir = 0; idir < 3; idir++) {
-        vft()->gather(*s->wf.spincontext(0), current[idir][0], global_current[idir]);
-    }
-
-    if ( s->ctxt_.onpe0() ) {
-
-        const int np0 = vft()->np0();
-        const int np1 = vft()->np1();
-        const int np2 = vft()->np2();
-
-        for(int idir = 0; idir < 3; idir++) {
-
-            std::ofstream os;
-
-            switch(idir) {
-            case 0:
-                os.open(("x" + filename + ".cube").c_str());
-                break;
-            case 1:
-                os.open(("y" + filename + ".cube").c_str());
-                break;
-            case 2:
-                os.open(("z" + filename + ".cube").c_str());
-                break;
+            for(int ip = 0; ip < vft()->np012loc(); ip++) {
+                total_current[idir] += volume_element*current[idir][ispin][ip];
             }
 
-            // write header and atoms
-            os << "Created " << isodate() << " by qbox-" << release() << endl;
-            os << endl;
+            wf_.spincontext(ispin)->dsum('c', 1, 1, &total_current[idir], 1);
 
-            int natoms = s->atoms.size();
-            D3vector a0 = s->atoms.cell().a(0);
-            D3vector a1 = s->atoms.cell().a(1);
-            D3vector a2 = s->atoms.cell().a(2);
-            os << natoms << " " << -0.5*(a0+a1+a2) << endl;
-
-            //write unit cell
-            os << np0 << " " << a0/np0 << endl;
-            os << np1 << " " << a1/np1 << endl;
-            os << np2 << " " << a2/np2 << endl;
-            const int nsp = s->atoms.nsp();
-            for ( int is = 0; is < nsp; is++ ) {
-                Species* sp = s->atoms.species_list[is];
-                const int z = sp->atomic_number();
-                const int na = s->atoms.na(is);
-                for ( int ia = 0; ia < na; ia++ ) {
-                    Atom *ap = s->atoms.atom_list[is][ia];
-                    D3vector pos =  ap->position();
-                    while((pos-(a0 + a1 + a2)/2.0)*a0/length(a0) > 1) pos -= a0;
-                    while((pos-(a0 + a1 + a2)/2.0)*a0/length(a0) < 0) pos += a0;
-                    while((pos-(a0 + a1 + a2)/2.0)*a1/length(a1) > 1) pos -= a1;
-                    while((pos-(a0 + a1 + a2)/2.0)*a1/length(a1) < 0) pos += a1;
-                    while((pos-(a0 + a1 + a2)/2.0)*a2/length(a2) > 1) pos -= a2;
-                    while((pos-(a0 + a1 + a2)/2.0)*a2/length(a2) < 0) pos += a2;
-
-                    os << setprecision(5);
-                    os << z << " " << ((double) z) << " " << pos << endl;
-                }
+            // kN term, not sure if needed
+            /*
+            for ( int ispin = 0; ispin < wf_.nspin(); ispin++){
+              for ( int ikp = 0; ikp < wf_.nkp(); ikp++ ){
+                cout << "kpt " << ikp << endl;
+                double wt = wf_.weight(ikp)/wf_.weightsum();
+                cout << "weight = " << wt << endl;
+                total_current[idir] += wt * wf_.sd(ispin, ikp)->kpoint()[idir]*energy_functional.hamil_cd()->nelectrons();
+              }
             }
+            */
 
-
-            os.setf(ios::scientific,ios::floatfield);
-            os << setprecision(5);
-            for ( int i = 0; i < np0; i++ ) {
-                const int ip = (i + np0/2 ) % np0;
-                for ( int j = 0; j < np1; j++ ) {
-                    const int jp = (j + np1/2 ) % np1;
-                    for ( int k = 0; k < np2; k++ ) {
-                        const int kp = (k + np2/2 ) % np2;
-                        os << setw(13) << global_current[idir][ip+np0*(jp+np1*kp)];
-                        if ( ( k % 6 ) == 5 )
-                            os << '\n';
-                    }
-                    if ( ( np2 % 6 ) != 0 )
-                        os << '\n';
-                }
-            }
-
-            os.close();
+            if(energy_functional.vp) total_current[idir] += energy_functional.vp->value()[idir]*energy_functional.hamil_cd()->nelectrons();
 
         }
 
     }
-}
 
-void CurrentDensity::plot_vtk(const Sample * s, const std::string & filename) {
-    using namespace std;
-    Base64Transcoder xcdr;
+    // TODO: Reduce total current over spin
+    assert(wf_.nspin() == 1);
 
-    std::vector<std::vector<double> > global_current(3);
-
-    for(int idir = 0; idir < 3; idir++) {
-        vft()->gather(*s->wf.spincontext(0), current[idir][0], global_current[idir]);
+    if (output) {
+        if ( wf_.context().onpe0() ) {
+            std::cout << "  total_electronic_current:\t" << std::fixed << std::setw( 20 ) << std::setprecision( 12 ) << total_current[0] << '\t' << total_current[1] << '\t' << total_current[2] << std::endl;
+        }
     }
 
-    if ( s->ctxt_.onpe0() ) {
 
-        const int np0 = vft()->np0();
-        const int np1 = vft()->np1();
-        const int np2 = vft()->np2();
+}
 
-        D3vector a0 = s->atoms.cell().a(0);
-        D3vector a1 = s->atoms.cell().a(1);
-        D3vector a2 = s->atoms.cell().a(2);
 
-        std::ofstream os;
+////////////////////////////////////////////////////////////////////
+void CurrentDensity::twfr(valarray<valarray<valarray<complex<double>>>>& gkswfr,valarray<valarray<complex<double>>>& kswfr,const vector<valarray<double>>& indexAll) const{
+  Wavefunction gwf(wf_);
+  for(int idir = 0; idir < 3; idir++){
+    for ( int ispin = 0; ispin < wf_.nspin(); ispin++){
+      for ( int ikp = 0; ikp < wf_.nkp(); ikp++ ){
 
-        os.open((filename + ".vtk").c_str(), ios::binary);
+        const int ngwloc = wf_.sd(ispin, ikp)->basis().localsize();
+        const int mloc = wf_.sd(ispin, ikp)->c().mloc();
 
-        // write header and atoms
-        os << "# vtk DataFile Version 2.0" << endl;
-        os << "Created " << isodate() << " by " << release() << endl;
-        os << "BINARY" << endl;
-        os << "DATASET STRUCTURED_POINTS" << endl;
-        os << "DIMENSIONS\t" << np0 << '\t' << np1 << '\t' << np2 << endl;
-        os << "ORIGIN\t" << -a0[0]/2.0 << "\t" << -a1[1]/2.0 << "\t" << -a2[2]/2.0 << "\t" << endl;
-        os << "SPACING\t" << a0[0]/np0 << '\t' << a1[1]/np1 << '\t' << a2[2]/np2 << endl;
-        os << "POINT_DATA\t" << np0*np1*np2 << endl;
-        os << "SCALARS current double 3" << endl;
-        os << "LOOKUP_TABLE default" << endl;
+        for ( int n = 0; n < wf_.sd(ispin, ikp)->nstloc(); n++ ){
+          for ( int ig = 0; ig < ngwloc; ig++ ){
+            gwf.sd(ispin, ikp)->c()[ig + mloc*n] = std::complex<double>(0.0, 1.0)*wf_.sd(ispin, ikp)->basis().kpgx_ptr(idir)[ig]*wf_.sd(ispin, ikp)->c()[ig + mloc*n];
+          }
+        }
+        if (idir==0) wf_.sd(ispin, ikp)->tmpwfr(*ft(ispin, ikp), *gwf.sd(ispin, ikp),gkswfr,kswfr,indexAll);
+        else wf_.sd(ispin, ikp)->tmpwfr(*ft(ispin, ikp), *gwf.sd(ispin, ikp),gkswfr,indexAll,idir);
+      }
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////
+void CurrentDensity::plot(const Sample * s, const std::string & filename){
+  using namespace std;
 
-        for ( int k = 0; k < np2; k++ ) {
-            const int kp = (k + np2/2 ) % np2;
+  std::vector<std::vector<double> > global_current(3);
 
-            for ( int j = 0; j < np1; j++ ) {
-                const int jp = (j + np1/2 ) % np1;
+  for(int idir = 0; idir < 3; idir++) {
+    vft()->gather(*s->wf.spincontext(0), current[idir][0], global_current[idir]);
+  }
 
-                for ( int i = 0; i < np0; i++ ) {
-                    const int ip = (i + np0/2 ) % np0;
+  if ( s->ctxt_.onpe0() )
+    {
 
-                    for(int idir = 0; idir < 3; idir++) {
-                        double value = global_current[idir][ip + np0*(jp + np1*kp)];
+      const int np0 = vft()->np0();
+      const int np1 = vft()->np1();
+      const int np2 = vft()->np2();
+      
+      for(int idir = 0; idir < 3; idir++){
+
+	std::ofstream os;
+
+	switch(idir){
+	case 0:
+	  os.open(("x" + filename + ".cube").c_str());
+	  break;
+	case 1:
+	  os.open(("y" + filename + ".cube").c_str());
+	  break;
+	case 2:
+	  os.open(("z" + filename + ".cube").c_str());
+	  break;
+	}
+
+	// write header and atoms
+	os << "Created " << isodate() << " by qbox-" << release() << endl;
+	os << endl;
+      
+	int natoms = s->atoms.size();
+	D3vector a0 = s->atoms.cell().a(0);
+	D3vector a1 = s->atoms.cell().a(1);
+	D3vector a2 = s->atoms.cell().a(2);
+	os << natoms << " " << -0.5*(a0+a1+a2) << endl;
+      
+	//write unit cell
+	os << np0 << " " << a0/np0 << endl;
+	os << np1 << " " << a1/np1 << endl;
+	os << np2 << " " << a2/np2 << endl;
+	const int nsp = s->atoms.nsp();
+	for ( int is = 0; is < nsp; is++ )
+	  {
+	    Species* sp = s->atoms.species_list[is];
+	    const int z = sp->atomic_number();
+	    const int na = s->atoms.na(is);
+	    for ( int ia = 0; ia < na; ia++ )
+	      {
+		Atom *ap = s->atoms.atom_list[is][ia];
+		D3vector pos =  ap->position();
+		while((pos-(a0 + a1 + a2)/2.0)*a0/length(a0) > 1) pos -= a0;
+		while((pos-(a0 + a1 + a2)/2.0)*a0/length(a0) < 0) pos += a0;
+		while((pos-(a0 + a1 + a2)/2.0)*a1/length(a1) > 1) pos -= a1;
+		while((pos-(a0 + a1 + a2)/2.0)*a1/length(a1) < 0) pos += a1;
+		while((pos-(a0 + a1 + a2)/2.0)*a2/length(a2) > 1) pos -= a2;
+		while((pos-(a0 + a1 + a2)/2.0)*a2/length(a2) < 0) pos += a2;
+		
+		os << setprecision(5);
+		os << z << " " << ((double) z) << " " << pos << endl;
+	      }
+	  }
+
+
+	os.setf(ios::scientific,ios::floatfield);
+	os << setprecision(5);
+	for ( int i = 0; i < np0; i++ )
+	  {
+	    const int ip = (i + np0/2 ) % np0;
+	    for ( int j = 0; j < np1; j++ )
+	      {
+		const int jp = (j + np1/2 ) % np1;
+		for ( int k = 0; k < np2; k++ )
+		  {
+		    const int kp = (k + np2/2 ) % np2;
+		    os << setw(13) << global_current[idir][ip+np0*(jp+np1*kp)];
+		    if ( ( k % 6 ) == 5 )
+		      os << '\n';
+		  }
+		if ( ( np2 % 6 ) != 0 )
+		  os << '\n';
+	      }
+	  }
+
+	os.close();
+
+      }
+
+    }
+}
+
+void CurrentDensity::plot_vtk(const Sample * s, const std::string & filename){
+  using namespace std;
+  Base64Transcoder xcdr;
+  
+  std::vector<std::vector<double> > global_current(3);
+  
+  for(int idir = 0; idir < 3; idir++) {
+    vft()->gather(*s->wf.spincontext(0), current[idir][0], global_current[idir]);
+  }
+
+  if ( s->ctxt_.onpe0() ) {
+
+    const int np0 = vft()->np0();
+    const int np1 = vft()->np1();
+    const int np2 = vft()->np2();
+    
+    D3vector a0 = s->atoms.cell().a(0);
+    D3vector a1 = s->atoms.cell().a(1);
+    D3vector a2 = s->atoms.cell().a(2);
+    
+    std::ofstream os;
+    
+    os.open((filename + ".vtk").c_str(), ios::binary);
+    
+    // write header and atoms
+    os << "# vtk DataFile Version 2.0" << endl;
+    os << "Created " << isodate() << " by " << release() << endl;
+    os << "BINARY" << endl;
+    os << "DATASET STRUCTURED_POINTS" << endl;
+    os << "DIMENSIONS\t" << np0 << '\t' << np1 << '\t' << np2 << endl;
+    os << "ORIGIN\t" << -a0[0]/2.0 << "\t" << -a1[1]/2.0 << "\t" << -a2[2]/2.0 << "\t" << endl;
+    os << "SPACING\t" << a0[0]/np0 << '\t' << a1[1]/np1 << '\t' << a2[2]/np2 << endl;
+    os << "POINT_DATA\t" << np0*np1*np2 << endl;
+    os << "SCALARS current double 3" << endl;
+    os << "LOOKUP_TABLE default" << endl;
+
+    for ( int k = 0; k < np2; k++ ) {
+      const int kp = (k + np2/2 ) % np2;
+      
+      for ( int j = 0; j < np1; j++ ) {
+	const int jp = (j + np1/2 ) % np1;
+
+	for ( int i = 0; i < np0; i++ ) {
+	  const int ip = (i + np0/2 ) % np0;
+	  
+	  for(int idir = 0; idir < 3; idir++) {
+	    double value = global_current[idir][ip + np0*(jp + np1*kp)];
 #ifndef WORDS_BIGENDIAN
-                        //Convert to big endian
-                        xcdr.byteswap_double(1, &value);
+	    //Convert to big endian	    
+	    xcdr.byteswap_double(1, &value);
 #endif
-                        os.write((char *)&value, sizeof(double));
-                    }
-                }
-            }
-        }
-
-        os.close();
-
+	    os.write((char *)&value, sizeof(double));
+	  }
+	}
+      }
     }
+      
+    os.close();
+    
+  }
 
 }
